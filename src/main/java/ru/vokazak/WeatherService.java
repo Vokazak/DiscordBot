59561919cfb15629e30e251ac9f1c9bbbe9e1d59
oleg_kazakov <olkazak21@gmail.com>
2021-05-08 23:27:52 +0300

    s

--- src/main/java/ru/vokazak/WeatherService.java
+++ src/main/java/ru/vokazak/WeatherService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherService {
-
     public String getWeatherForecast() {
         return "It's gonna snow a lot. Brace yourselves, the winter is coming.";
     }
